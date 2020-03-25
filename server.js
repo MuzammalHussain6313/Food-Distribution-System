22aa74728b981a6884c9ecb79dbbffbde1f7bb8b
MuzammalHussain6313 <51987198+MuzammalHussain6313@users.noreply.github.com>
2020-03-25 08:38:19 +0500

    server is updated.

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-// Hello word server
+const express = require('express');
+const app = express();
+
+app.get('*', (req, res) => {
+    res.send('hello world');
+});
+
+app.listen(3000, () => {
+    console.log('Express application running on localhost:3000');
+});
