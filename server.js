f047ea0ae9d93cd49b7fa48b51c017d81285f285
MuzammalHussain6313 <51987198+MuzammalHussain6313@users.noreply.github.com>
2020-03-26 21:52:08 +0500

    Update Server
    database url was changed, local to online database.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/fyp', {useNewUrlParser: true}, ()=>console.log('connected'));
+//mongoose.connect('mongodb://localhost:27017/fyp', {useNewUrlParser: true}, ()=>console.log('connected'));
 
-// mongoose.connect('mongodb+srv://muzammal6313:ashrafi9885@cluster0-4hc2l.mongodb.net/studentDB?retryWrites=true&w=majority',
-//     {useNewUrlParser: true}).then(() => console.log('connected')).catch(err =>{
-//     console.log(err);});
+mongoose.connect('mongodb+srv://muzammalbsse:muzammalbsse@online-cluster-kylqc.mongodb.net/foodDB?retryWrites=true&w=majority,
+     {useNewUrlParser: true}).then(() => console.log('connected')).catch(err =>{
+     console.log(err);});
 
 const cors = require('cors');
 const express = require('express');
