150caa9718f6e1343a6d92e914d468d90d1a641f
MuzammalHussain6313 <51987198+MuzammalHussain6313@users.noreply.github.com>
2020-03-26 21:53:55 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/fyp', {useNewUrlParser: true}, ()=>console.log('connected'));
 
-mongoose.connect('mongodb+srv://muzammalbsse:muzammalbsse@online-cluster-kylqc.mongodb.net/foodDB?retryWrites=true&w=majority,
+mongoose.connect('mongodb+srv://muzammalbsse:muzammalbsse@online-cluster-kylqc.mongodb.net/foodDB?retryWrites=true&w=majority',
      {useNewUrlParser: true}).then(() => console.log('connected')).catch(err =>{
      console.log(err);});
 
